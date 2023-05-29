7ac0b7ec331e7f85bf97044f4266e0d854c5f4dc
ChrisClevenger <aenguscoen@gmail.com>
2023-05-29 11:38:04 -0500

    Edit js to reach desired sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(lowEnd, highEnd) {
+    if (lowEnd > highEnd) {
+        [lowEnd, highEnd] = [highEnd, lowEnd]; 
+    }
+    else if (lowEnd < 0) {
+        return ("ERROR"); 
+    }
+    else if (highEnd < 0) {
+        return ("ERROR"); 
+    }
 
+    else if (Number(lowEnd) !== lowEnd || Number(highEnd) !== highEnd) {
+        return ("ERROR"); 
+    }
+    const intArray = []; 
+    for (let i=lowEnd; i <= highEnd; i++) {
+        intArray.push(i); 
+    }
+    let sumArray = 0; 
+    for (let i=0; i < intArray.length; i++) {
+        sumArray += intArray [i]; 
+    }
+    return (sumArray); 
 };
+
+sumAll(10, [90,  1]); 
 
 // Do not edit below this line
 module.exports = sumAll;
