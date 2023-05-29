5cb4ad86c9df6943cb1f830e233b9464be1dd85c
ChrisClevenger <aenguscoen@gmail.com>
2023-05-29 14:31:50 -0500

    Edit js to convert temperatures accurately

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  return Math.round((degreesF - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  let roundedFahrenheit = Math.round(((degreesC * 1.8) + 32) * 10) / 10; 
+  return roundedFahrenheit; 
 };
+
+convertToFahrenheit(73.2); 
 
 // Do not edit below this line
 module.exports = {
