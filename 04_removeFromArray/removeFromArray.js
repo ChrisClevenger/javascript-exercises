b9b47fb778edcef7db76de895a0e3c9ce95cc82d
ChrisClevenger <aenguscoen@gmail.com>
2023-05-29 10:19:50 -0500

    Write js to remove certain values from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    values.forEach((value) => {
+    let index = arr.indexOf(value);
+    if (index > -1) 
+      arr.splice(index, 1);
+    }); 
+    return arr;
+  }
 
-};
+  removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
