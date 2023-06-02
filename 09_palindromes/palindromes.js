b46e1d3104b638a2fed8b448de5d3f1361fdace1
ChrisClevenger <aenguscoen@gmail.com>
2023-06-02 16:37:15 -0500

    Build palindrome js function to pass tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const noPunctuation = input.replace(/[^a-zA-Z0-9]/g, ''); 
+
+    const regInput = noPunctuation.toLowerCase(); 
+
+    function reverseString(str) {
+        return str.split("").reverse().join("");
+    }; 
+
+    const revInput = reverseString(regInput); 
+    
+    if (regInput == revInput) {
+        return true; 
+    } else {
+        return false; 
+    }
 
 };
 
